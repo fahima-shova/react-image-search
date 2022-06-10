c6df9307cec5b03b194113fa4a864e6ddd51a261
fahima-shova <fahima.shova06@gmail.com>
2022-06-10 22:28:12 +0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+const App = () => {
+    return (
+    <div className='ui container' style={{ margin: '10 px'}}>
+        <SearchBar />
+    </div>
+    );
+}
+
+export default App;
